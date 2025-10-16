5341bcad02375670c3011fa0df9550c04a38186b
unknown <98l39416oo@gmail.com>
2025-10-16 16:44:47 +0100

    fix env

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,4 @@
-from .category import CategoryViewSet
+from .category import CategoryPublicViewSet, CategoryAdminViewSet
 from .customer import CustomerViewSet, UserViewSet
-from .product import ProductViewSet, StandardResultsSetPagination
+from .product import ProductViewSet, StandardResultsSetPagination, ProductOperation
 from .order import OrderViewSet
--- sajcarpet/settings.py
+++ sajcarpet/settings.py
@@ -21,9 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=Csv())
+print("DEBUG =", DEBUG)
+print("ALLOWED_HOSTS =", ALLOWED_HOSTS)
 
 
 # Application definition
